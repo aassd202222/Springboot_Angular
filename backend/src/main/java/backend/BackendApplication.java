02ab800f51dd7ac3a657c18cb81a1cff2df5e08f
JAZZA132 <66157474+JAZZA132@users.noreply.github.com>
2021-07-01 16:15:27 +0800

    Add files via upload

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -2,9 +2,18 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
